8f86ba0a2aaf6058c0d6aa9cb616032f9e6d4df9
ika0109 <ikhboloy97@gmail.com>
2025-03-15 00:51:20 +0900

    feat:MIT Task - H

--- src/train.js
+++ src/train.js
@@ -1,7 +1,21 @@
-function engKattaQiymatIndeksi(a) {
-    const engKattaQiymat = Math.max(...a);
-    return a.findIndex(son => son === engKattaQiymat);
-  }
+function getPositive(sonlar) {
+  const musbatSonlar = sonlar.filter(num => num > 0);
+  return musbatSonlar.join('');
+}
+
+
+console.log(getPositive([1, -4, 2])); 
+  
+
+
+
+
+
+
+// function engKattaQiymatIndeksi(a) {
+//     const engKattaQiymat = Math.max(...a);
+//     return a.findIndex(son => son === engKattaQiymat);
+//   }
   
   
-  console.log(engKattaQiymatIndeksi([5, 21, 12, 21, 8])); 
+//   console.log(engKattaQiymatIndeksi([5, 21, 12, 21, 8])); 
