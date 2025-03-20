49bf32ebe4e057e3bc72df5671f397d8a59869b9
ika0109 <ikhboloy97@gmail.com>
2025-03-20 19:07:19 +0900

    feat: create restaurant router get and post endpoints

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -5,14 +5,12 @@ import MemberService from "../models/Member.service";
 const restaurantController: T = {};
 
 restaurantController.goHome = (req: Request, res: Response) => {
-
   try {
-   
     console.log("goHome");
-         //LOGIC
+    //LOGIC
     //SERVICE MODEL
     //...
-    res.send("Home Page");
+    res.send("Home Page"); // send | json | redirect | end | render
   } catch (err) {
     console.log("Error, goHome", err);
   }
@@ -27,6 +25,8 @@ restaurantController.getLogin = (req: Request, res: Response) => {
   }
 };
 
+
+
 restaurantController.getSignup = (req: Request, res: Response) => {
   try {
     console.log("getSignup");
@@ -36,4 +36,23 @@ restaurantController.getSignup = (req: Request, res: Response) => {
   }
 };
 
+restaurantController.processLogin = (req: Request, res: Response) => {
+  try {
+    console.log("processLogin");
+    res.send("DONE");
+  } catch (err) {
+    console.log("Error, processLogin", err);
+  }
+};
+
+restaurantController.processSignup = (req: Request, res: Response) => {
+  try {
+    console.log("processSignup");
+    res.send("DONE");
+  } catch (err) {
+    console.log("Error, processSignup", err);
+  }
+};
 export default restaurantController;
+
+
--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -1,11 +1,34 @@
+// import express from "express";
+// const routerAdmin = express.Router();
+// import restaurantController from "./controllers/restaurant.controller";
+
+// routerAdmin.get("/", restaurantController.goHome);
+
+// routerAdmin
+//   .get("/login", restaurantController.getLogin)
+//   .post("/login", restaurantController.processLogin);
+
+// routerAdmin
+//   .get("./signup", restaurantController.getSignup)
+//   .post("./signup", restaurantController.processSignup);
+
+// export default routerAdmin;
+
 import express from "express";
-const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
+const routerAdmin = express.Router();
 
+// Restaurant
 routerAdmin.get("/", restaurantController.goHome);
+routerAdmin
+  .get("/login", restaurantController.getLogin)
+  .post("/login", restaurantController.processLogin);
+routerAdmin
+  .get("/signup", restaurantController.getSignup)
+  .post("/signup", restaurantController.processSignup);
 
-routerAdmin.get("/login", restaurantController.getLogin);
+// Product
 
-routerAdmin.get("./signup", restaurantController.getSignup);
+//User
 
 export default routerAdmin;
