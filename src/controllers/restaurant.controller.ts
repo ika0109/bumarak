5230ecad33d4df581f4301fe9b6d96a2b8858c02
ika0109 <ikhboloy97@gmail.com>
2025-03-27 03:23:45 +0900

    feat: frontend related css, js and img creation

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -11,7 +11,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
   try {
     console.log("goHome");
 
-    res.send("Home Page");
+    res.render("home");
     //send | json | redirect | end | render
   } catch (err) {
     console.log("Error goHOme", err);
@@ -20,15 +20,15 @@ restaurantController.goHome = (req: Request, res: Response) => {
 restaurantController.getSignup = (req: Request, res: Response) => {
   try {
     console.log("Signup Page");
-    res.send("Signup Page");
-  } catch (err) {
+    res.render("signup")
+} catch (err) {
     console.log("Error getSignup", err);
   }
 };
 restaurantController.getLogin = (req: Request, res: Response) => {
   try {
     console.log("getLogin");
-    res.send("Login Page");
+    res.render("login");
   } catch (err) {
     console.log("Error getLogin", err);
   }
--- src/server.ts
+++ src/server.ts
@@ -1,48 +1,29 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-
-
 import mongoose from "mongoose";
 import app from "./app";
 
 mongoose
-.connect(process.env.MONGO_URL as string, {})
-.then((data) => {
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
     console.log("MongoDB connection succeed");
     const PORT = process.env.PORT ?? 3003;
     app.listen(PORT, function () {
-        console.log(`The server is running successfully on port: ${PORT}`)
-    })
-})
-.catch((err) => console.log ("ERROR on collection MongoDB", err))
+      console.info(`The server is running successfully on port: ${PORT}`);
+      console.info(`Admin project on http://localhost${PORT}/admin\n`);
+    });
+  })
+  .catch((err) => console.log("ERROR on collection MongoDB", err));
 
 // CLUSTER => DATABASE => COLLECTION => DOCUMENT
 
 // console.log("PORT:", process.env.PORT);
 // console.log("MONGO_URL:", process.env.MONGO_URL);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //Architectural pattern: MVC, Dependency Injection, MVP
 
 //Design pattern: Middleware, Decotar
-
-
-
 
 // console.log("EXUCUTED!");
 
