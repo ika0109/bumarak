c5f323b1cacfcb04d943f6a1333a3897f635071c
ika0109 <ikhboloy97@gmail.com>
2025-03-28 23:49:23 +0900

    fix: create sessions on login and signup process

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { Collection } from "./../node_modules/connect-mongodb-session/node_modules/mongodb/src/collection";
 import express from "express";
 import path from "path";
 import router from "./router";
@@ -5,68 +6,39 @@ import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
 
-import session from "express-session"
+import session from "express-session";
 import ConnectMongoDB from "connect-mongodb-session";
 
-const MongoDBStore = ConnectMongoDB(session)
+const MongoDBStore = ConnectMongoDB(session);
 const store = new MongoDBStore({
-    uri: String(process.env.MONGO_UR),
-    collection: 'sessions'
-})
-
-
-
-// / 1 - ENTRANCE /
+  uri: String(process.env.MONGO_URL),
+  collection: "sessions",
+});
+// 1-ENTRANCE/
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
+// 2-SESSION/
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000 * 360 * 3, // 3h
+    },
+    store: store,
 
-// / 2 - SESSIONS /
-app.use (
-    session({secret: String(process.env.SESSION_SECRET),
-        cookie: {
-          maxAge: 1000 * 3600 * 3, // 3 h
-        },
-        store: store,
-        
-        resave: true,
-        saveUninitialized: true})
-)
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-// / 3 - VIEWS /
+//3-VIEW/
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
-// / 4 - ROUTERS /
-app.use("/admin", routerAdmin); // EJS
-app.use("/", router); // React
+//4-ROUTERS/;
+app.use("/admin", routerAdmin); //BSSR: EJS
+app.use("/", router); // SPA: REACT
 
 export default app;
-
-// import express from "express";
-// import path from "path";
-// import router from "./router";
-// import routerAdmin from "./router-admin";
-// import morgan from "morgan";
-// import { MORGAN_FORMAT } from "./libs/config";
-
-// /** 1-ENTRANCE **/
-// const app = express();
-// app.use(express.static(path.join(__dirname, "public")));
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(morgan(MORGAN_FORMAT));
-
-// /** 2-SESSIONS **/
-
-// /** 3-VIEWS **/
-// app.set("views", path.join(__dirname, "views"));
-// app.set("views engine", "ejs");
-
-// /**4-ROUTERS **/
-// //BSSR : EJS
-// app.use("/admin", routerAdmin);
-// app.use("/", router); // SPA :REACT
-// export default app;
--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
-import { LoginInput, MemberInput } from "../libs/types/member";
+import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 // bssr
 const memberService = new MemberService();
@@ -20,8 +20,8 @@ restaurantController.goHome = (req: Request, res: Response) => {
 restaurantController.getSignup = (req: Request, res: Response) => {
   try {
     console.log("Signup Page");
-    res.render("signup")
-} catch (err) {
+    res.render("signup");
+  } catch (err) {
     console.log("Error getSignup", err);
   }
 };
@@ -34,7 +34,10 @@ restaurantController.getLogin = (req: Request, res: Response) => {
   }
 };
 
-restaurantController.processSignup = async (req: Request, res: Response) => {
+restaurantController.processSignup = async (
+  req: AdminRequest,
+  res: Response
+) => {
   try {
     console.log("processSignup");
 
@@ -44,21 +47,30 @@ restaurantController.processSignup = async (req: Request, res: Response) => {
 
     const result = await memberService.processSignup(newMember); // CALL
 
-    res.send(result);
+    req.session.member = result;
+    req.session.save(function () {
+      res.send(result);
+    });
   } catch (err) {
     console.log("Error procssesSignup", err);
     res.send(err);
   }
 };
 
-restaurantController.processLogin = async (req: Request, res: Response) => {
+restaurantController.processLogin = async (
+  req: AdminRequest,
+  res: Response
+) => {
   try {
     console.log("procssesLogin");
     console.log("body", req.body);
     const input: LoginInput = req.body;
 
     const result = await memberService.processLogin(input);
-    res.send(result);
+    req.session.member = result;
+    req.session.save(function () {
+      res.send(result);
+    });
   } catch (err) {
     console.log("Error procssesLogin", err);
     res.send(err);
@@ -66,3 +78,4 @@ restaurantController.processLogin = async (req: Request, res: Response) => {
 };
 
 export default restaurantController;
+ 
