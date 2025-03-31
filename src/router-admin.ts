0968e7775f9e46796a4faada1280b5b16c131da2
ika0109 <ikhboloy97@gmail.com>
2025-03-31 22:05:23 +0900

    feat:create product controller and product apis

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -16,22 +16,26 @@
 
 import express from "express";
 import restaurantController from "./controllers/restaurant.controller";
+import productController from "./controllers/product.controller";
 const routerAdmin = express.Router();
 
 // Restaurant
 routerAdmin.get("/", restaurantController.goHome); // 1 qadam
 routerAdmin
   .get("/login", restaurantController.getLogin)
-  .post("/login",restaurantController.processLogin);
+  .post("/login", restaurantController.processLogin);
 
 routerAdmin
   .get("/signup", restaurantController.getSignup)
   .post("/signup", restaurantController.processSignup);
 
-  routerAdmin.get("/logout", restaurantController.logout);
-  routerAdmin.get("/check-me", restaurantController.checkAuthSession);
+routerAdmin.get("/logout", restaurantController.logout);
+routerAdmin.get("/check-me", restaurantController.checkAuthSession);
 
 // Product
+routerAdmin.get("/product/all", productController.getAllProducts);
+routerAdmin.post("/product/create", productController.createNewProduct);
+routerAdmin.post("/product/:id", productController.updateChosenProduct);
 
 //User
 
