374aca7dcfc5ee7adb34533f4e1c3db6af9305a5
ika0109 <ikhboloy97@gmail.com>
2025-05-03 13:05:46 +0900

    feat: develop getRestaurant rest API

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import memberController from "./controllers/member.controller";
 const router = express.Router();
 import uploader from "./libs/utils/uploader";
 /** Member */
+router.get("/member/restaurant", memberController.getRestaurant);
 router.post("/member/login", memberController.login);
 router.post("/member/signup", memberController.signup);
 router.post(
