33f19c0eba2d4ed1153251fbc733dd4cffdb8b40
ika0109 <ikhboloy97@gmail.com>
2025-05-09 23:11:59 +0900

    feat: develop getProducts business logic

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import express from "express";
 import memberController from "./controllers/member.controller";
 const router = express.Router();
 import uploader from "./libs/utils/uploader";
+import productController from "./controllers/product.controller";
 /** Member */
 router.get("/member/restaurant", memberController.getRestaurant);
 router.post("/member/login", memberController.login);
@@ -25,6 +26,6 @@ router.post(
 router.get("/member/top-users", memberController.getTopUsers);
 
 /** Product */
-
+router.get("/product/all", productController.getProducts);
 /** Order */
 export default router;
